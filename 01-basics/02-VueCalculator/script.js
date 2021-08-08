a97749231f903f49045234ee23e55f5e746acc4e
naziradenushka <59218086+naziradenushka@users.noreply.github.com>
2021-08-08 13:06:16 +0500

    vue calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,32 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      number1: 0,
+      number2: 0,
+      operation: 'sum',
+      result: 0,
+    };
+  },
+  computed: {
+    getResult() {
+      let answer = 0;
+      if (this.operation == 'sum') {
+        answer = +this.number1 + +this.number2;
+      } else if (this.operation == 'subtract') {
+        answer = +this.number1 - +this.number2;
+      } else if (this.operation == 'multiply') {
+        answer = +this.number1 * +this.number2;
+      } else if (this.operation == 'divide') {
+        if (this.number2 == 0) {
+          answer = 'На 0 делить нельзя';
+        } else {
+          answer = +this.number1 / +this.number2;
+        }
+      }
+      return answer;
+    },
+  },
+}).mount('#app');
