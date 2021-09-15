a99b0fb599c6180f34dbccdf426fff805e80fe6f
naziradenushka <59218086+naziradenushka@users.noreply.github.com>
2021-09-15 23:03:56 +0500

    04-vue-cli the toaster

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
+    <template v-for="(toast, index) in toasts" :key="index">
+      <div v-if="toast.show" class="toast" :class="toast.class">
+        <ui-icon class="toast__icon" :icon="toast.icon" />
+        <span>{{ toast.msg }}</span>
+      </div>
+    </template>
+    <!-- <div lass="toast toast_success">
       <ui-icon class="toast__icon" icon="check-circle" />
       <span>Success Toast Example</span>
     </div>
@@ -8,7 +14,7 @@
     <div class="toast toast_error">
       <ui-icon class="toast__icon" icon="alert-circle" />
       <span>Error Toast Example</span>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -17,8 +23,41 @@ import UiIcon from './UiIcon';
 
 export default {
   name: 'TheToaster',
-
   components: { UiIcon },
+  data() {
+    return {
+      toasts: [],
+    };
+  },
+  methods: {
+    cancel() {
+      this.toasts.splice(0, 1);
+    },
+    success(message) {
+      this.toasts.push({
+        show: true,
+        class: 'toast_success',
+        msg: message,
+        icon: 'check-circle',
+      });
+      setTimeout(() => {
+        // console.log('s', id);
+        this.cancel();
+      }, 5000);
+    },
+    error(message) {
+      this.toasts.push({
+        show: true,
+        class: 'toast_error',
+        msg: message,
+        icon: 'alert-circle',
+      });
+      setTimeout(() => {
+        // console.log('e', id);
+        this.cancel();
+      }, 5000);
+    },
+  },
 };
 </script>
 
