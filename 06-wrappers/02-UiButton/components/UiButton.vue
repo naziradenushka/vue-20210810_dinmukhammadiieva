0ce90c35d9e7d91c46eb3d115680fc8a4506416f
naziradenushka <59218086+naziradenushka@users.noreply.github.com>
2021-12-19 15:28:13 +0500

    06-wrappers 02-UiButton

--- 06-wrappers/02-UiButton/components/UiButton.vue
+++ 06-wrappers/02-UiButton/components/UiButton.vue
@@ -1,10 +1,35 @@
 <template>
-  <button class="button button_secondary button_block">BUTTON</button>
+  <component
+    :is="block ? 'div' : tag"
+    class="button"
+    :class="[`button_${variant}`, { button_block: block }]"
+    :type="block ? null : type"
+  >
+    <slot />
+  </component>
 </template>
 
 <script>
 export default {
   name: 'UiButton',
+  props: {
+    tag: {
+      type: String,
+      default: 'button',
+    },
+    block: {
+      type: Boolean,
+      default: false,
+    },
+    variant: {
+      type: String,
+      default: 'secondary',
+    },
+    type: {
+      type: String,
+      default: 'button',
+    },
+  },
 };
 </script>
 
